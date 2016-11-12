086cd2f0ffacc00bba663144694fed99149618b0
Michael Holtz <michael.holtz@teledyne.com>
2016-11-11 18:17:43 -0800

    Added more stuff

---
+++ MMManager/GameObjects/SharedTicTacToeBoardData.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MMManager.GameInterfaces;
+namespace MMManager.GameObjects
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Serializable]
+    public class SharedTicTacToeBoardData
+    {
+        //TODO - Share ENUMS
+        public enum GameState
+        {
+            Waiting=0,
+            Playing=1,
+            GameOver=2
+            
+        }
+        public enum MessageCode
+        {
+            Start=0,
+            Join=1,
+            Watch=2,
+            Play=3,
+            Move=4,
+            Bomb=5,
+            Win=6,
+            Lose=7,
+            Draw=8,
+            OK=9,
+            Reset=10,
+            NewGame=11,
+            RefreshGameList=12,
+            RemoveGame,
+            LeaveGame
+                
+                
+
+        };
+        public List<Player> Players;
+
+        public GameState State { get; set; }
+        public MessageCode Message { get; set; }
+        public string MessageString { get; set; }
+        public string MessageValue { get; set; }
+
+        public string MessageSender { get; set; }
+
+        public SharedTicTacToeBoardData()
+        {
+            Players = new List<Player>(); // Default to no players.
+        }
+
+    }
+    [Serializable]
+    public class Player
+    {
+        public String PlayerName { get; set; }
+        public Char PlayerSymbol { get; set; }
+        public bool PlayerTurn { get; set; }
+        public bool PlayerWon { get; set; }
+    }
+
+}
---
+++ MMManager/IMessageRelay.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MMManager.GameObjects;
+namespace MMManager
+{
+    /// <summary>
+    /// Contract for having a form configured to send messages and being able to send as it.
+    /// </summary>
+    public interface IMessageRelay
+    {
+        void SendTicTacToeMessage(string gameName, string memberName, SharedTicTacToeBoardData generatedBoardData);
+    }
+}
